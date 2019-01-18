720e3301a37ee9f02ed324fe3211f87b30e886da
Jim Ritchey <jim0ritchey@gmail.com>
2019-01-17 21:00:50 -0500

    Added base SendGrid functionality, as in it sends a test email.

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+// using SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'jim0ritchey@gmail.com',
+  from: 'test@example.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg).catch(function(err) {
+    console.log("Broke at: " + err);
+});
