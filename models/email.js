b5de2c8a52ab4b7a62b3f398b894ebaa396476b3
Michael Grill <Ultramar@Michaels-MacBook-Pro.local>
2019-01-24 18:36:06 -0500

    EditingEmailTable

--- models/email.js
+++ models/email.js
@@ -2,6 +2,7 @@ module.exports = function(sequelize, DataTypes) {
     var Email = sequelize.define("Email", {
       From: DataTypes.STRING,
       Subject: DataTypes.STRING,
+      To: DataTypes.String,
       Body: DataTypes.TEXT
     });
     return Email;
