3898c4cedfb69e9a702675c11a9cb369ef6472c5
Michael Grill <Ultramar@Michaels-MacBook-Pro.local>
2019-01-24 20:38:03 -0500

    TablesWillCreateNow

--- models/email.js
+++ models/email.js
@@ -2,9 +2,9 @@ module.exports = function(sequelize, DataTypes) {
     var Email = sequelize.define("Email", {
       From: DataTypes.STRING,
       Subject: DataTypes.STRING,
-      To: DataTypes.String,
+      To: DataTypes.STRING,
       Body: DataTypes.TEXT,
-      SendDate: Datatype.DATE
+      SendDate: DataTypes.DATE
     });
     return Email;
   };
