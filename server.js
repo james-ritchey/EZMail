ac8db5e4bc2b247c1aa464e6cc97673cc6b75701
Jim Ritchey <jim0ritchey@gmail.com>
2019-02-01 14:34:09 -0500

    Organizing files for heroku deployment

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ var PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
+//app.use(express.static("public"));
 
 
 // Routes
 require("./routes/apiRoutes")(app);
-//require("./routes/htmlRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
 
